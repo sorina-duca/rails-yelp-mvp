6da582f347fbecdcaff5236310a25d29ca4d39f3
Sorina Duca <sorina.duca@gmail.com>
2020-06-23 21:01:12 +0200

    create backend

---
+++ app/controllers/restaurants_controller.rb
@@ -0,0 +1,34 @@
+class RestaurantsController < ApplicationController
+before_action :get_restaurant, only: [:show]
+
+  def index
+    @restaurants = Restaurant.all
+  end
+
+  def show
+  end
+
+  def new
+    @restaurant = Restaurant.new
+  end
+
+  def create
+    @restaurant = Restaurant.new(strong_params_restaurant)
+    @restaurant.save
+    if @restaurant.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def get_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def strong_params_restaurant
+    params.require(:restaurant).permit(:name, :rating, :address, :phone_number, :category)
+  end
+end
